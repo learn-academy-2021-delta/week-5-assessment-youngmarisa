1fce0e65bb36ee23d55d401956c21b06f00fafe3
youngmarisa <marisayoung2013@gmail.com>
2021-10-10 17:13:26 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,11 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { createReadStream } = require("fs")
+
+// const { expect } = require("@jest/globals")
+
+// const { array } = require("prop-types")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -24,8 +30,57 @@ var secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 
+// declare a test function called codeMessage that takes in a string
+describe ("codeMessage", () => {
+    // write a test method that describes what the test will do
+    // this test will convert the letters 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0 in the string
+    it ("takes in a string and returns a new string that converted the letters 'a' to 4  , 'e' to 3, 'i' to 1, and 'o' to 0'", () => {
+        // write an expect method for each input
+        //the function should output a new string that has converted the specific letters to the corresponding code letter or number
+        expect(codeMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(codeMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(codeMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+
+
 
 // b) Create the function that makes the test pass.
+
+// create a function
+// convert string to an array using .split(""); creates an array where each value in the array is a letter from the string
+// create a new function that takes in the array iterates through each value using .map()
+// write if/elseif/else statement to convert the specified letters to their respected code number
+// for each value : if i === 'a' return 4, if i === 'e' return 3, if i === 'i' return 1, if i === 'o' return 0; else return i
+// convert the array back to a string using .join("")
+
+function codeMessage(string){
+    var arrayCode = string.split("")
+    const code = (arrayCode) => {
+        return arrayCode.map(value => {
+            if (value === 'a' || value === 'A'){
+                return '4'
+            }
+            else if (value === 'e' || value === 'E'){
+                return '3'
+            }
+            else if (value === 'i' || value === 'I'){
+                return '1'
+            }
+            else if (value === 'o' || value === 'O'){
+                return '0'
+            }
+            else {
+                return value
+            }  
+        })
+    }
+    return code(arrayCode).join("")
+}
+    
+codeMessage(secretCodeWord1)
+codeMessage(secretCodeWord2)
+codeMessage(secretCodeWord3)
 
 
 
@@ -40,11 +95,32 @@ var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 var letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
-
+// declare a test function called newFruit that takes in a string and an array parameter
+describe ('newFruit', () => {
+    // write a test method that describes what the test will do
+    // this function will take in an array of words and a single letter and return a new array with the words containing that particular letter
+    it ("takes in an array of words and a single letter and returns all the words that contain that particular letter", () => {
+        // write an expect method for each input
+        // use the expect.arrayContaining(array) to have the output be a subset of the received array
+        // the expected output will be the array filtered with only the words that contain that particular letter
+        expect(["Apple", "Banana", "Plum", "Orange", "Kiwi"]).toEqual(expect.arrayContaining(["Apple", "Banana", "Orange"]))
+        expect(["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]).toEqual(expect.arrayContaining(["Cherry", "Blueberry", "Peach"]))
+    })
+    
+})
 
 // b) Create the function that makes the test pass.
 
+// create a function that takes in a letter string and an array of strings
+// use .filter to return only the truthy values
+// filter through the array to determine if each string value in the array contains the letter string passed in
+// use .includes to see if the letter is in that string value when the letter is lowercase OR when it is uppercase 
 
+var newFruit = (str, arr) => {
+    return arr.filter(fruit => fruit.includes(str) || fruit.includes(str.toUpperCase()))
+}
+newFruit(letterA, arrayOfWords1)
+newFruit(letterE, arrayOfWords2)
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
@@ -57,6 +133,60 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+// declare a test function called fullHouse that takes in an array
+describe ("fullHouse", () => {
+    // write a test method that describes what the function is testing
+    // the test should look at each value in the array to determine if the array is a full house
+    it ("takes in an array of 5 numbers and determines whether or not the array has exactly one pair and one three of a kind", () => {
+        // write an expect method for each input
+        // if the input array is a full house: has one pair and one three of a kind, return true 
+        // if the input is not a full house, return false
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
 
+    })
+})
 
 // b) Create the function that makes the test pass.
+
+// create a function that takes in an array
+// for a full house, you only need two unique cards
+// find how many times the first number in the array occurs
+// try to see how many times the first value occurs in the array
+// increment the count of the first number in the array for how ever many times it occurs
+// fine the second unique number in the array
+// find the next humber in the array that is not equal to the first number (second unique number)
+// if the count of the first unique number is not two or three return false
+// find the cound of the second unique number
+// if the count of this number is not 2 or 3 then return false 
+// if the count of the first unique number is 2 or 3 AND the count of the second unique number is 2 or 3 then return true 
+
+const fullHouse = (array) => {
+    let count = [0,0]
+    let secondNum = 0
+    for (i=0; i<5; i++){
+        if (array[0] == array[i]){
+            count[0]++
+        }
+        else {
+            secondNum = array[i]
+        }
+    }
+    if ((count[0] !== 2) && (count[0] !== 3)){
+        return false
+    }
+    for (i=0; i<5; i++){
+        if (array[i] === secondNum){
+            count[1]++
+        }
+    }
+    if ((count[1] !==2) && (count[1] !==3)){
+        return false
+    }
+    return true
+}
+
+fullHouse(hand1)
+fullHouse(hand2)
+fullHouse(hand3)
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Are (most) Ruby methods accessors or mutators?
 
-  Your answer:
+  Your answer: Most ruby methods are accessors, meaning they do not change the original array whereas mutators will completely modify the input variable. However, the bang operator can be used with a method to make it a mutator method.  
 
-  Researched answer:
+  Researched answer: Ruby accessor methods return the orginal variables' values. Ruby's accessor methods consist of attr_reader, attr_writer, and attr_accessor. Using these accessor methods, references the behavior of the instance variable only once; therefore, it is no longer referenced directly throughout the program. Attr_reader allows you to get the value of the instance variable, attr_writer allows you to set the value, and attr_accessor allows you to do both. Attr_accessor is a shortcut to set up getter and setter methods in Ruby. It is a way to set and get values from outside the class. 
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: Ruby blocks are anonymous functions that use the keywords do/end or use curly braces {} to indicate a code block. The code within the do/end or {} block are executed when passed through a Ruby method. 
 
-  Researched answer:
+  Researched answer: Blocks are assigned a name and consist of chunks of code. The code is enclosed with either a do/end or {}. Do/end is primarily used for code that is multiple lines long whereas the curly brakets are used for single line code. Blocks can have multiple paramaters such as values, indexes, and arrays. Blocks are passed into a method. 
 
 
 
 3. What is a gem?
 
-  Your answer:
+  Your answer: A gem is a package manager for the Ruby language.  This package manager allows code in Ruby to be easily shared. 
 
-  Researched answer:
+  Researched answer: A gem is a library of code functionality. Gems can be used in Ruby applications to extend or modify functionality. They are most commonly used between developers to distribute reusable functionality for Ruby applications. Each gem has a name, version and platform. They are organized by lib, test (sepc), Rakefile, README, and gemspecs. The code for the gem lies in the lib directory. The testing aspect is stored in the test directory. Rakefile automates the tests and generates the code. The documentation for the code is in the README file. Lastly, the gemspec or gem specification holds the information about the gem's author, the purpose for the gem, and the creation time. 
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: A relational database is a database that uses rows and columns in a table to sort data. Databases can consist of multiple tables of data that relate to one another. Non-relational databases don't follow a table based way to store data. 
 
-  Researched answer:
+  Researched answer: Relational database represent data in tables where each row holds a primary key and the columns of the table how the data information. Relational databases provide access to data that are related to one another and it makes the relationship between each data point clear. Non-relational databases is a storage model that is more optimized for the data type it is storing. Non-relational databases are more selective in the type of data they support as well as how the data can be queried. There are four tipes of NoSQL databases: document oriented databases, key-value stores, wide-column stores, and graph stores. 
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: Primary keys in databases give each data row a unique ID. This is important because rows may have similar or even the same data information but the primary key given to each row seperates them from being mixed up or confused with each other. 
 
-  Researched answer:
+  Researched answer: A primary key is used in relational databases to uniquely identify each table row. This allows the data within the table to be quickly identified since a table canot have more than one primary key. These keys cannot contain null values. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: Representational State Transfer (REST) routing is a standard for creating resuable and efficient routes by asserting thw routes that complete common actions have names that accuretly depict the action it is performing. It is a route that provides mapping from HTTP to controller CRUD actions. 
 
-2. JSON:
+2. JSON: JSON or JavaScript Object notation is a text-based format that is used to represent structured data. It is primarily used for data transmission in web applications. 
 
-3. ERB:
+3. ERB: Embedded Ruby or ERB is a file structure that allows developers to code HTML with Ruby code embedded in the view file. 
 
-4. Params:
+4. Params: Params also known as URL parameters or query strings. They are used to dynamically modify the view or to query the database by passing in additional information into a controller method. 
 
-5. API:
+5. API: Application Programming Interface is a software that allows two seperate applications or two seperate computers to communicate with each other. 
